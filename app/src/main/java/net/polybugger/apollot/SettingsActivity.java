7ce4a87057e534f4fda229e05a401e78cc25f296
Poly Bug <polybugger@gmail.com>
2016-02-13 07:44:00 +0800

    Added Toolbar to Settings Activity

--- app/src/main/java/net/polybugger/apollot/SettingsActivity.java
+++ app/src/main/java/net/polybugger/apollot/SettingsActivity.java
@@ -5,14 +5,18 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         if(savedInstanceState == null)
-            getSupportFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsPreferenceFragment()).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new SettingsPreferenceFragment()).commit();
     }
 
     public static class SettingsPreferenceFragment extends PreferenceFragmentCompat {
